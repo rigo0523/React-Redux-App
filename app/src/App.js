4fa0ec214cd92876b364153940b67f81148dfc71
Rigo <limon1285@gmail.com>
2020-09-22 22:29:51 -0500

    update

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Project 3 react redux</h1>
+    </div>
+  );
+}
+
+export default App;
